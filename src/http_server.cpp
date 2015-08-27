159ca3aaebd2fce7be55ee04467a2ca10f9cad6a
450255457 <450255457@qq.com>
2015-08-27 15:12:34 +0800

    08271512

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -143,6 +143,53 @@ static void dump_request_cb(struct evhttp_request *req, void *arg)
 static void send_document_cb(struct evhttp_request *req, void *arg)
 {
 	printf("Run send_document_cb.\n");
+	char output[2048] = "\0";
+	char tmp[1024];
+
+	//获取客户端请求的URI(使用evhttp_request_uri或直接req->uri)
+	const char *uri;
+	uri = evhttp_request_uri(req);
+	sprintf(tmp, "uri=%s\n", uri);
+	strcat(output, tmp);
+
+//	sprintf(tmp, "uri=%s\n", req->uri);
+//	strcat(output, tmp);
+	char *decoded_uri;
+	decoded_uri = evhttp_decode_uri(uri);
+	sprintf(tmp, "decoded_uri=%s\n", decoded_uri);
+	strcat(output, tmp);
+
+	//解析URI的参数(即GET方法的参数)
+	struct evkeyvalq params;
+	evhttp_parse_query(decoded_uri, &params);
+	sprintf(tmp, "q=%s\n", evhttp_find_header(&params, "q"));
+	strcat(output, tmp);
+	sprintf(tmp, "s=%s\n", evhttp_find_header(&params, "s"));
+	strcat(output, tmp);
+	free(decoded_uri);
+
+	//获取POST方法的数据
+	//char *post_data = (char *)EVBUFFER_DATA(req->input_buffer);
+	//sprintf(tmp, "post_data=%s\n", post_data);
+	//strcat(output, tmp);
+
+	/*
+	具体的：可以根据GET/POST的参数执行相应操作，然后将结果输出
+	...
+	*/
+
+	/* 输出到客户端 */
+
+	//HTTP header
+	//evhttp_add_header(req->output_headers, "Server", "MYHTTPD_SIGNATURE");
+	//evhttp_add_header(req->output_headers, "Content-Type", "text/plain; charset=UTF-8");
+	//evhttp_add_header(req->output_headers, "Connection", "close");
+	//输出的内容
+	struct evbuffer *buf;
+	buf = evbuffer_new();
+	evbuffer_add_printf(buf, "It works!\n%s\n", output);
+	evhttp_send_reply(req, HTTP_OK, "OK", buf);
+	evbuffer_free(buf);
 //	struct evbuffer *evb = NULL;
 //	const char *docroot = "/usr/share/nginx/html/JiaTuanWeb";
 //	const char *uri = evhttp_request_get_uri(req);
@@ -273,54 +320,6 @@ static void send_document_cb(struct evhttp_request *req, void *arg)
 //	if (evb)
 //		evbuffer_free(evb);
 
-	char output[2048] = "\0";
-	char tmp[1024];
-
-	//获取客户端请求的URI(使用evhttp_request_uri或直接req->uri)
-	const char *uri;
-	uri = evhttp_request_uri(req);
-	sprintf(tmp, "uri=%s\n", uri);
-	strcat(output, tmp);
-
-	sprintf(tmp, "uri=%s\n", req->uri);
-	strcat(output, tmp);
-	//decoded uri
-	char *decoded_uri;
-	decoded_uri = evhttp_decode_uri(uri);
-	sprintf(tmp, "decoded_uri=%s\n", decoded_uri);
-	strcat(output, tmp);
-
-	//解析URI的参数(即GET方法的参数)
-	struct evkeyvalq params;
-	evhttp_parse_query(decoded_uri, &params);
-	sprintf(tmp, "q=%s\n", evhttp_find_header(&params, "q"));
-	strcat(output, tmp);
-	sprintf(tmp, "s=%s\n", evhttp_find_header(&params, "s"));
-	strcat(output, tmp);
-	free(decoded_uri);
-
-	//获取POST方法的数据
-	char *post_data = (char *)EVBUFFER_DATA(req->input_buffer);
-	sprintf(tmp, "post_data=%s\n", post_data);
-	strcat(output, tmp);
-
-	/*
-	具体的：可以根据GET/POST的参数执行相应操作，然后将结果输出
-	...
-	*/
-
-	/* 输出到客户端 */
-
-	//HTTP header
-	evhttp_add_header(req->output_headers, "Server", "MYHTTPD_SIGNATURE");
-	evhttp_add_header(req->output_headers, "Content-Type", "text/plain; charset=UTF-8");
-	evhttp_add_header(req->output_headers, "Connection", "close");
-	//输出的内容
-	struct evbuffer *buf;
-	buf = evbuffer_new();
-	evbuffer_add_printf(buf, "It works!\n%s\n", output);
-	evhttp_send_reply(req, HTTP_OK, "OK", buf);
-	evbuffer_free(buf);
 }
 
 int	main(int argc, char **argv)
