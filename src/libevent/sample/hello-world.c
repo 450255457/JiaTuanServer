4df0e5861bdbb7e9f5e0bd98f4e2af9f982bd41f
450255457 <450255457@qq.com>
2015-12-29 11:20:55 +0800

    12291120

--- src/libevent/sample/hello-world.c
+++ src/libevent/sample/hello-world.c
@@ -1,9 +1,11 @@
 /*
-  This exmple program provides a trivial server program that listens for TCP
-  connections on port 9995.  When they arrive, it writes a short message to
-  each client connection, and closes each connection once it is flushed.
+This exmple program provides a trivial server program that listens for TCP
+connections on port 9995.  When they arrive, it writes a short message to
+each client connection, and closes each connection once it is flushed.
 
-  Where possible, it exits cleanly in response to a SIGINT (ctrl-c).
+Where possible, it exits cleanly in response to a SIGINT (ctrl-c).
+
+gcc -g -o hello-world hello-world.c -levent
 */
 
 
@@ -27,16 +29,14 @@
 
 static const char MESSAGE[] = "Hello, World!\n";
 
-static const int PORT = 9995;
+static const int PORT = 8090;
 
-static void listener_cb(struct evconnlistener *, evutil_socket_t,
-    struct sockaddr *, int socklen, void *);
+static void listener_cb(struct evconnlistener *, evutil_socket_t, struct sockaddr *, int socklen, void *);
 static void conn_writecb(struct bufferevent *, void *);
 static void conn_eventcb(struct bufferevent *, short, void *);
 static void signal_cb(evutil_socket_t, short, void *);
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	struct event_base *base;
 	struct evconnlistener *listener;
@@ -58,23 +58,20 @@ main(int argc, char **argv)
 	sin.sin_family = AF_INET;
 	sin.sin_port = htons(PORT);
 
-	listener = evconnlistener_new_bind(base, listener_cb, (void *)base,
-	    LEV_OPT_REUSEABLE|LEV_OPT_CLOSE_ON_FREE, -1,
-	    (struct sockaddr*)&sin,
-	    sizeof(sin));
+	listener = evconnlistener_new_bind(base, listener_cb, (void *)base, LEV_OPT_REUSEABLE | LEV_OPT_CLOSE_ON_FREE, -1, (struct sockaddr*)&sin, sizeof(sin));
 
 	if (!listener) {
 		fprintf(stderr, "Could not create a listener!\n");
 		return 1;
 	}
-
+	//#define evsignal_new(b, x, cb, arg)		event_new((b), (x), EV_SIGNAL | EV_PERSIST, (cb), (arg))
 	signal_event = evsignal_new(base, SIGINT, signal_cb, (void *)base);
 
 	if (!signal_event || event_add(signal_event, NULL)<0) {
 		fprintf(stderr, "Could not create/add a signal event!\n");
 		return 1;
 	}
-
+	printf("Server is running on %d...", PORT);
 	event_base_dispatch(base);
 
 	evconnlistener_free(listener);
@@ -85,11 +82,9 @@ main(int argc, char **argv)
 	return 0;
 }
 
-static void
-listener_cb(struct evconnlistener *listener, evutil_socket_t fd,
-    struct sockaddr *sa, int socklen, void *user_data)
+static void listener_cb(struct evconnlistener *listener, evutil_socket_t fd, struct sockaddr *sa, int socklen, void *user_data)
 {
-	struct event_base *base = user_data;
+	struct event_base *base = (struct event_base *)user_data;
 	struct bufferevent *bev;
 
 	bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
@@ -105,8 +100,7 @@ listener_cb(struct evconnlistener *listener, evutil_socket_t fd,
 	bufferevent_write(bev, MESSAGE, strlen(MESSAGE));
 }
 
-static void
-conn_writecb(struct bufferevent *bev, void *user_data)
+static void conn_writecb(struct bufferevent *bev, void *user_data)
 {
 	struct evbuffer *output = bufferevent_get_output(bev);
 	if (evbuffer_get_length(output) == 0) {
@@ -115,24 +109,23 @@ conn_writecb(struct bufferevent *bev, void *user_data)
 	}
 }
 
-static void
-conn_eventcb(struct bufferevent *bev, short events, void *user_data)
+static void conn_eventcb(struct bufferevent *bev, short events, void *user_data)
 {
 	if (events & BEV_EVENT_EOF) {
 		printf("Connection closed.\n");
-	} else if (events & BEV_EVENT_ERROR) {
+	}
+	else if (events & BEV_EVENT_ERROR) {
 		printf("Got an error on the connection: %s\n",
-		    strerror(errno));/*XXX win32*/
+			strerror(errno));/*XXX win32*/
 	}
 	/* None of the other events can happen here, since we haven't enabled
-	 * timeouts */
+	* timeouts */
 	bufferevent_free(bev);
 }
 
-static void
-signal_cb(evutil_socket_t sig, short events, void *user_data)
+static void signal_cb(evutil_socket_t sig, short events, void *user_data)
 {
-	struct event_base *base = user_data;
+	struct event_base *base = (struct event_base *)user_data;
 	struct timeval delay = { 2, 0 };
 
 	printf("Caught an interrupt signal; exiting cleanly in two seconds.\n");
